72e92603f2580c6cc79c69225cf71fa25a0f6562
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-05-31 12:39:40 -0500

    intro

--- 01-leds-and-buttons/01-blinking-led.js
+++ 01-leds-and-buttons/01-blinking-led.js
@@ -1,1 +1,13 @@
 // Our code here!
+const five = require('johnny-five');
+const Tessel = require('tessel-io');
+const board = new five.Board({
+    io: new Tessel()
+});
+
+board.on('ready', () => {
+    // const led = new five.Led('a0');
+    const led = new five.Led('a5');
+    // led.blink(500);
+    led.pulse(800);
+});
